67c0122c5a6b9f22a6feea7b0568b75cbe31de7b
jennwintr <jennwintr@gmail.com>
2023-09-21 10:00:39 -0400

    added js and current time

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  
   
      // Get the current day using Day.js
      const currentDay = dayjs().format('dddd');
@@ -32,11 +10,19 @@ $(function () {
   
      // Get the current date using Day.js
   
-     const currentDate = dayjs().format('MM-DD-YYYY');
+     const currentDate = dayjs().format('MMMM DD, YYYY');
   
      // Display the current date in the placeholder element
   
      $('#currentDate').text(`${currentDate}`);
+
+     // Get the current time using Day.js
+  
+     const currentTime = dayjs().format('HH:mm A', true);
+  
+     // Display the current time in the placeholder element
+  
+     $('#currentTime').text(`${currentTime}`);
   
      // Iterate through each time block
      $('.time-block').each(function() {
